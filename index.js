6887e7a97e81f64deda858648cdcec72f8f86514
Akash Routhu <38069976-akashrouthu@users.noreply.replit.com>
2024-11-29 10:24:38 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// You are given an object product with the properties id, name, price, and category. Perform the following operations:
+// 1.	Access the price property and print it.
+// 2.	Modify the category property to “Electronics”.
+// 3.	Add a new property inStock to the product object with the value true.
+// 4.	Delete the id property from the product object.
+var product={
+  id:1,
+  name:"laptop",
+  price:20000,
+  category:"electronics"
+}
+console.table(product);
+console.log(product.price);
+console.table(product);
+product.category="Electronics";  
+console.table(product);
+product.inStock=true;
+console.table(product);
+delete product.id;
+console.table(product);
